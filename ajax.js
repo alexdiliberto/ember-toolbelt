e5c316154bc9256941348808e7bef617ecb446ed
Alex DiLiberto <alexdiliberto@server.fake>
2013-09-22 21:17:39 -0400

    Added Ember Ajax utility

---
+++ ajax.js
@@ -0,0 +1,20 @@
+/*
+* Ember RSVP Promise wrapper for $.ajax()
+*
+* USAGE: App.ajax('/articles/user/' + user_id);
+*/
+App.ajax = function(url, options) {
+  return Ember.RSVP.Promise(function(resolve, reject) {
+    var options = options || {};
+
+    options.success = function(data) {
+      Ember.run(null, resolve, data);
+    };
+
+    options.error = function(jqXHR, status, error) {
+      Ember.run(null, reject, arguments);
+    };
+
+    $.ajax(url, options);
+  });
+};

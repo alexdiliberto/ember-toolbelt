99f7446c503c276eaa3cfbaefcc77e21d782ba0f
Alex DiLiberto <alexdiliberto@server.fake>
2013-12-07 23:20:53 -0500

    Added google-analytics.js integration helper

---
+++ google-analytics.js
@@ -0,0 +1,11 @@
+/**
+  Ember Google Analytics integration
+*/
+App.Router.reopen({
+  didTransition: function(infos) {
+    this._super(infos);
+    Ember.run.next(function() {
+      _gaq.push(['_trackPageView'], window.location.pathname);
+    });
+  }
+});

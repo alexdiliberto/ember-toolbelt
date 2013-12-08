58579b8a06a218a78a87303fbbba70d131545986
Alex DiLiberto <alexdiliberto@server.fake>
2013-12-07 23:44:25 -0500

    Update to add window.location.hash

--- google-analytics.js
+++ google-analytics.js
@@ -5,7 +5,7 @@ App.Router.reopen({
   didTransition: function(infos) {
     this._super(infos);
     Ember.run.next(function() {
-      _gaq.push(['_trackPageView'], window.location.pathname);
+      _gaq.push(['_trackPageView'], window.location.pathname + window.location.hash);
     });
   }
 });

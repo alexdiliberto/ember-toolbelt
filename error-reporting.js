4fac185e14860485bf803518cd0f289e8ac42c6d
Alex DiLiberto <alex@alexdiliberto.com>
2014-01-25 22:06:20 -0500

    Update error-reporting.js
    Added method to log all errors that are not immediately handled in promises

--- error-reporting.js
+++ error-reporting.js
@@ -7,3 +7,19 @@ Ember.onerror = function(error) {
     otherInformation: 'exception message'
   });
 };
+
+
+/**
+  Log all errors that are not immediately handled in promises
+*/
+Ember.RSVP.on('error', function(error) {
+  // optionally, only for errors
+  // if (error instanceof Error) { console.assert(false, error); }
+  
+  console.assert(false, error);
+  
+  if (error && error.stack) {
+    // console.error provides clickable stack-traces in chrome debugger
+    console.error(error.stack);
+  }
+});

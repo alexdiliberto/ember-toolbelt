329904ee984e67414c0c37a7f5b3e5ab0e9298b0
Alex DiLiberto <alex@alexdiliberto.com>
2014-01-29 18:14:35 -0500

    Update ajax.js
    Added default for options.type

--- ajax.js
+++ ajax.js
@@ -7,15 +7,15 @@ App.ajax = function(url, options) {
   return new Ember.RSVP.Promise(function(resolve, reject) {
     var options = options || {};
 
-    options.dataType = "json";
-
+    options.type = options.type || "get";
+    options.dataType = options.dataType || "json";
     options.success = function(data) {
       Ember.run(null, resolve, data);
     };
-
     options.error = function(jqXHR, status, error) {
       Ember.run(null, reject, arguments);
     };
+
 
     $.ajax(url, options);
   });

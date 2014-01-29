d7aeadb8d3f609ac412cedde26c2cdcc7ed6d03d
Alex DiLiberto <alex@alexdiliberto.com>
2014-01-29 18:15:00 -0500

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -6,7 +6,7 @@
 App.ajax = function(url, options) {
   return new Ember.RSVP.Promise(function(resolve, reject) {
     var options = options || {};
-
+    
     options.type = options.type || "get";
     options.dataType = options.dataType || "json";
     options.success = function(data) {
@@ -15,7 +15,6 @@ App.ajax = function(url, options) {
     options.error = function(jqXHR, status, error) {
       Ember.run(null, reject, arguments);
     };
-
 
     $.ajax(url, options);
   });

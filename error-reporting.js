b5765a755c01bf55dd865332b64c537ccc2bc01e
Alex DiLiberto <alex@alexdiliberto.com>
2014-06-09 23:16:59 -0400

    Update error-reporting.js

--- error-reporting.js
+++ error-reporting.js
@@ -23,3 +23,27 @@ Ember.RSVP.on('error', function(error) {
     console.error(error.stack);
   }
 });
+
+/**
+  Also consider more thorough error hanling **(UNTESTED)**
+  
+  `report_error` is a function that takes an Exception and deals with logging and reporting.
+  
+  // Trap exceptions from within Ember run loop
+  Ember.onerror = report_error;
+  
+  // Trap unhandled RSVP promise failures
+  Ember.RSVP.configure('onerror', report_error);
+  
+  // Trap unhandled failures during routing
+  App.ApplicationRoute = Ember.Route.extend({
+      actions: { error: report_error }
+  });
+  
+  // Safety net to report any untrapped exceptions on browsers
+  // that respect window.onerror.  Currently, failures from within 
+  // Backburner callbacks will end up here. This is inferior to the 
+  // other handlers because the stack trace is missing by the 
+  // time the exception gets here.
+  window.onerror = report_error;
+*/
